7aac8ec6e0805618369df1d4a60e2c8803fe1464
sophiamirrashidi <smirrash@usc.edu>
2026-05-03 18:25:45 -0700

    Syncing

--- train.py
+++ train.py
@@ -141,7 +141,7 @@ def main(args):
         print('Epoch: {} \tResnet Training Loss: {:.6f}'.format(epoch + 1, train_loss))
 
     print("Saving model...")
-    torch.save(model.state_dict(), "./resnet.pt")
+    torch.save(model.state_dict(), f"./resnet_l2_{args.weight_decay}.pt")
 
     h1.remove()
     h2.remove()
--- train.sh
+++ train.sh
@@ -5,5 +5,6 @@ python train.py \
     --lr 0.01 \
     --probe_lr 0.01 \
     --momentum 0.9 \
-    --weight_decay 1e-1 \
+    --weight_decay 0.0 \
     --num_epochs 10
+
